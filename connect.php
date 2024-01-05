e125274e2b6830342020904819f620bd1b4ec989
Rah-7 <rahigirmam16@gmail.com>
2024-01-05 23:33:48 +0300

    connect database

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+//variables
+$localhost = "localhost";
+$user = "root";
+$password = "usbw";
+$database = "institutions-address";
+
+//create a database connection
+
+$mysqli = new mysqli($localhost, $user, $password, $database);
+if ($mysqli->connect_errno) { 
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+
+?>
